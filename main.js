3438b435c52f312f1eb2bae76b19f1e2f5e63697
OmerFI <omergumushane@hotmail.com>
2021-10-20 00:06:20 +0300

    Add improvements

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 content = document.querySelector(".main-div .content");
+childsOfContent = content.children;
 
-console.log(content);
+leftBarNest = document.querySelector(".main-div .left-bar .left-bar-nest");
+h1s = leftBarNest.querySelectorAll("h1");
+
+h1Ids = [];
+for (let index = 0; index < h1s.length; index++) {
+  h1Ids[index] = h1s[index].id;
+}
+
+leftBarNest.addEventListener("click", (e) => {
+  id = e.target.id;
+
+  if (h1Ids.includes(id)) {
+    Array.from(childsOfContent).forEach((childContent) => {
+      if (childContent.id == id) {
+        childContent.classList.remove("inactive");
+      } else {
+        childContent.classList.add("inactive");
+      }
+    });
+  }
+});
